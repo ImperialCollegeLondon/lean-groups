df06b3d832df6e270ae5a93472e3ea07bcea5309
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-07-08 14:33:20 +0100

    subgroup question

--- src/subgroup.lean
+++ src/subgroup.lean
@@ -2,4 +2,14 @@
 
 -- We're going to make one object which is all the subgroups of G.
 
--- comment
+-- first let's do some tests
+
+import group_theory.subgroup
+import algebra.group.hom
+
+example (G1 G2 : Type*) [group G1] [group G2]
+  (f : G1 → G2) [is_group_hom f] (H1 : set G1) [is_subgroup H1] :
+  is_subgroup (f '' H1) :=
+{ one_mem := _,
+  mul_mem := _,
+  inv_mem := _ }
